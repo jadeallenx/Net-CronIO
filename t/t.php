70eea72ff5b2bd0535a87b7d06ada0bb640e05a4
Mark Allen <mrallen1@yahoo.com>
2012-02-08 00:47:37 -0600

    Log remote IPs of visitors

--- t/t.php
+++ t/t.php
@@ -6,11 +6,12 @@
   $counter = intval($s);
   $counter++;
 
-  echo "<HTML><HEAD></HEAD><BODY><H1>This page has been visited $counter times!</H1><pre>";
+  echo "<HTML><HEAD></HEAD><BODY><H1>This page has been visited $counter times!</H1><ul>";
   echo file_get_contents("./visitors.txt");
-  echo "</pre></BODY></HTML>";
+  echo "</ul></BODY></HTML>";
 
   file_put_contents("./counter.txt", $counter);
-  file_put_contents("./visitors.txt", print_r($_REQUEST, TRUE), FILE_APPEND);
+  file_put_contents("./visitors.txt", "<li>". $_ENV['REMOTE_ADDR']. "</li>\n", FILE_APPEND);
 
 ?>
+
